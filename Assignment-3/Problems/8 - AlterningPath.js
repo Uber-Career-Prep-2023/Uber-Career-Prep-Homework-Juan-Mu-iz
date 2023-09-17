8536875279902d2d5a72d54beb9f39c250938f47
Juan Pablo de León <devleon00@gmail.com>
2023-09-17 15:25:30 -0600

    Add 8 probelm of assigment 3

--- Assignment-3/Problems/8 - AlterningPath.js
+++ Assignment-3/Problems/8 - AlterningPath.js
@@ -17,17 +17,83 @@
 
 ///////// PROCESS THINKING /////////
 
-//  Every time we add a new nuber the previous number are reapeted, when we add 10-11, we repeated the 0 and 1 after the new 1 added. So, if we can add the previous values to the new value we can create the new one in base of the previous. To pass the previous values we can use a queue to save them
+//  Use BFS to traverse the graph and keep track of the previous color, if the current color is the same as the previous color, don't add it to the queue. If the destination is found, return the path length
 
 ///////// CODING /////////
-function ReverseWords() {
+function buildAdjacencyList(edges) {
+	// Create an empty adjacency list
+    const adjacencyList = {};
 
-	return "";
+	// Iterate over the edges
+    for (let i = 0; i < edges.length; i++) {
+		// If the origin node isn't in the adjacency list, add it
+        if (!adjacencyList[edges[i][0]]) {
+            adjacencyList[edges[i][0]] = [];
+        }
+		// If the destination node isn't in the adjacency list, add it
+        if (!adjacencyList[edges[i][1]]) {
+            adjacencyList[edges[i][1]] = [];
+        }
+		// Add the destination node to the adjacency list of the origin node
+        adjacencyList[edges[i][0]].push([edges[i][1], edges[i][2]]);
+    }
+
+    return adjacencyList;
 }
 
+function bfs(graph, origin, destination) {
+	// Create a queue to keep track of nodes to visit
+    const queue = [];
+	// Add the origin node to the queue with a previous color of null and a path length of 0
+    queue.push([origin, null, 0]);
 
-// Time Complexity: O(n) It could be improve if we use a real queue ,the improvement would still be O(n) 
-// Space Complexity: O(n) 
+    while (queue.length > 0) {
+		// Dequeue the next node to visit
+        const [currentNode, previousColor, pathLength] = queue.shift();
 
-// Time: 20 minutes
-// Feeling: Easy, i quickly recognized the idea of the solution, I tried with adding the previous values with index. Founding a queue was the result of seeing the examples a little bit more, and see that every time its just adding at the end a 1 and a 0
+		// Iterate over the neighbors of the current node
+        for (const [neighbor, color] of graph[currentNode]) {
+			// Check if the previous color 
+            if (color !== previousColor) {
+                queue.push([neighbor, color, pathLength + 1]);
+				// Check if the neighbor is the destination node
+                if (neighbor === destination) {
+                    return pathLength + 1;
+                }
+            }
+        }
+    }
+
+    return -1;
+}
+
+function alternatingPath(graph, origin, destination) {
+	// If the origin and destination are the same, return 0
+    if (origin === destination) {
+        return 0;
+    }
+
+    const directedGraph = buildAdjacencyList(graph);
+    return bfs(directedGraph, origin, destination);
+}
+
+const graph = [
+    ["A", "B", "blue"],
+    ["A", "C", "red"],
+    ["B", "D", "blue"],
+    ["B", "E", "blue"],
+    ["C", "B", "red"],
+    ["D", "C", "blue"],
+    ["A", "D", "red"],
+    ["D", "E", "red"],
+    ["E", "C", "red"]
+];
+
+console.log(alternatingPath(graph, "A", "E")); // 4
+console.log(alternatingPath(graph, "E", "D")); // -1
+
+// Time Complexity: O(V + E) Visit every node and edge once
+// Space Complexity: O(V + E) Create an adjacency list with every node and edge
+
+// Time: 25 minutes
+// Feeling: Medium, I had to think a little bit about the BFS, but the rest was easy. I thought that i needed to keep track of the visited nodes, but I didn't need to.
